4419d28f2b4632b6a3afcec849d246e64ce42f47
drascom <drayhancolak@gmail.com>
2025-03-31 15:03:09 +0100

    folder organise

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+export const config = {
+    supabaseUrl: 'https://ykpqkrneoyynwxiyxvrh.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrcHFrcm5lb3l5bnd4aXl4dnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzQ3OTIsImV4cCI6MjA1ODE1MDc5Mn0.bGo86AuzRDDg_-fweDWH09lDTPbfT6Na7MUeVwpQVRs'
+};
